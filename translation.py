8fe94ed70654134b2d6a2fc96f0b42e3632956f6
saikatdas19082001 <54448492+saikatdas19082001@users.noreply.github.com>
2021-06-29 15:54:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ This code is only used for the purpose of getting APP ID from my.telegram.org
 if you don't trust this dev bot, just take it manually😁🙏
 """
     BEFORE_SUCC_LOGIN = "Code Received. Scarpping Web Pages..."
-    ERRED_PAGE = "Hadeh Error. Failed to get application id. \n\n contact : @DaedXn"
+    ERRED_PAGE = "Scrapping Error. Failed to get application id. \n\n contact : @DaedXn"
     CANCELLED_MESG = "Bye! Please / restart to repeat"
     IN_VALID_CODE_PVDED = "Enter the Telegram Code that you have received 😁🙏"
     IN_VALID_PHNO_PVDED = "Enter a Real Telegram Phone number 😁🙏 Use Format +(country code) Example +91(number)"
